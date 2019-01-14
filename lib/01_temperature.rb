60f690062d403bb6abc7733e362012a124355fca
LordGoblin <maxime78600@gmail.com>
2019-01-14 20:43:46 +0100

    ok

--- lib/01_temperature.rb
+++ lib/01_temperature.rb
@@ -1,25 +1,10 @@
 def ftoc(temp)
-	if temp == 32
-		temp = 0
-	elsif temp == 212
-		temp = 100
-	elsif temp == 98.6
-		temp = 37
-	elsif temp == 68
-		 temp = 20
-	end
+	temp = temp.to_f
+	temp = (temp-32)*5/9
 	return temp
 end
 
 def ctof(temp)
-	if temp == 0
-		temp = 32
-	elsif temp == 100
-		temp = 212
-	elsif temp == 20
-		temp = 68
-	elsif temp == 37
-		temp = 98.6 - 0.1
-	end
+	temp = (temp*1.8) + 32
 	return temp
 end
